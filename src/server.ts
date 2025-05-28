a31db347eabbeae818508764575f70509fda1628
Alejandro Fierro <alejandro.fierro@gmail.com>
2025-05-28 01:26:28 -0600

    latest fix
    Signed-off-by: Alejandro Fierro <alejandro.fierro@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -752,7 +752,7 @@ export class Server extends EventEmitter {
       '<soapenv:Envelope xmlns:soapenv="' +
       envelopeDefinition +
       '" ' +
-      'xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
+      'xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
       encoding +
       this.wsdl.xmlnsInEnvelope +
       ">";
